d5a8c24f53d248c787681893d45ba993c9799352
BenNewell32 <ben.m.newell@gmail.com>
2019-01-27 19:23:27 -0600

    navigate between tableau and oracle sections.

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -7,3 +7,21 @@ function showNewToAnalytics(){
       x.style.display = "none";
     }
 }
+
+function showTableau(){
+    var x = document.getElementById("newToAnalytics");
+      x.style.display = "none";
+      var y = document.getElementById("Oracle");
+      y.style.display = "none";
+      var z = document.getElementById("Tableau");
+      z.style.display = "block";
+}
+
+function showOracle(){
+    var x = document.getElementById("newToAnalytics");
+      x.style.display = "none";
+    var y = document.getElementById("Oracle");
+    y.style.display = "block";
+    var z = document.getElementById("Tableau");
+    z.style.display = "none";
+}
