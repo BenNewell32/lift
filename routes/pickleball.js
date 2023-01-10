0ef880dadfd5ac3306f2bd14d67d7a95456541fe
bennewell32 <ben.m.newell@gmail.com>
2023-01-09 20:02:43 -0600

    removing puppeteer

--- routes/pickleball.js
+++ routes/pickleball.js
@@ -4,29 +4,29 @@ const puppeteer = require("puppeteer");
 var CNPdata;
 
 // PULL CNP DATA//
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  // Goto page
-  await page.goto(
-    "https://www.reservecnp.com/overland-park/pickleball-indoor-court/"
-  );
-  // Scroll down
-  page.evaluate((_) => window.scrollBy(0, 1000));
-  // Wait a bit
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  // Get the src of the iframe
-  CNPdata = await page.evaluate(`$("#id_tm").find("option").text()`);
+// (async () => {
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   // Goto page
+//   await page.goto(
+//     "https://www.reservecnp.com/overland-park/pickleball-indoor-court/"
+//   );
+//   // Scroll down
+//   page.evaluate((_) => window.scrollBy(0, 1000));
+//   // Wait a bit
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   // Get the src of the iframe
+//   CNPdata = await page.evaluate(`$("#id_tm").find("option").text()`);
 
-  console.log(CNPdata);
-  await browser.close();
-})();
+//   console.log(CNPdata);
+//   await browser.close();
+// })();
 //////////////////
 
 router.get("/", async function (req, res, next) {
   res.render("pickleball", {
     title: "Ben and Yana Pickleball",
-    data: CNPdata,
+    // data: CNPdata,
   });
 });
 
